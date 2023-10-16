4bd4788b78527286dda703b1959cd218c814588f
Jussi Talaskivi <jptalask@gmail.com>
2023-10-16 12:28:58 +0300

    Update version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_openai_chat';
-$plugin->version = 2023082200;
+$plugin->version = 2023101600;
 $plugin->requires = 2020061513;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.7.0.jyu.2';
+$plugin->release = '1.7.0.jyu.3';
